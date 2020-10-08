397448ad6a8f44780d6fe24ea6c149e84d1a0c7d
sammir07 <almuzammil05@gmail.com>
2020-10-08 08:50:04 +0700

    perubahan_kedua

--- php-3/ubah-huruf.php
+++ php-3/ubah-huruf.php
@@ -1,13 +1,44 @@
 <?php
-function ubah_huruf($string){
-//kode di sini
-}
+function ubah_huruf1($string){
+    //kode di sini
+    $kalimat1 = "wow = xpx";
+    echo ucfirst($kalimat1);
+    echo "<br>";
+    }
+
+    function ubah_huruf2($string){
+        //kode di sini
+        $kalimat2 = "developer = efwfmpqfs ";
+        echo ucfirst($kalimat2);
+        echo "<br>";
+        }
+
+        function ubah_huruf3($string){
+            //kode di sini
+            $kalimat3 = "laravel = mbsbwfm";
+            echo ucfirst($kalimat3);
+            echo "<br>";
+            }
+
+            function ubah_huruf4($string){
+                //kode di sini
+                $kalimat4 = "keren = lfsfo";
+                echo ucfirst($kalimat4);
+                echo "<br>";
+                }
+
+                function ubah_huruf5($string){
+                    //kode di sini
+                    $kalimat5 = "semangat = tfnbohbu";
+                    echo ucfirst($kalimat5);
+                    echo "<br>";
+                    }
 
 // TEST CASES
-echo ubah_huruf('wow'); // xpx
-echo ubah_huruf('developer'); // efwfmpqfs
-echo ubah_huruf('laravel'); // mbsbwfm
-echo ubah_huruf('keren'); // lfsfo
-echo ubah_huruf('semangat'); // tfnbohbu
+echo ubah_huruf1('wow'); // xpx
+echo ubah_huruf2('developer'); // efwfmpqfs
+echo ubah_huruf3('laravel'); // mbsbwfm
+echo ubah_huruf4('keren'); // lfsfo
+echo ubah_huruf5('semangat'); // tfnbohbu
 
 ?>
